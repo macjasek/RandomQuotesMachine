1d3c39150dd9a3a4bee0fb6486501a18893bd2bb
macjasek <maciej.dobruchowski@ispro.com.pl>
2018-01-08 20:04:49 +0100

    Adding class for creating image from quote text. It is for share quote to facebook.

---
+++ RandomQuotesMachine/QuoteImage.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+
+namespace RandomQuotesMachine
+{
+    public class QuoteImage
+    {
+        public static Image DrawQuote(string text, string fontName, int fontSize)
+        {
+
+            Font drawFont = new Font("Arial", 16);
+
+            SizeF textSize = TextSizeCalculator(text, drawFont);
+
+            var img = new Bitmap((int)textSize.Width, (int)textSize.Height);
+
+            var drawing = Graphics.FromImage(img);
+
+
+
+            return img;
+        }
+
+        private static SizeF TextSizeCalculator(string text, Font fontToMeasure)
+        {
+            Image img = new Bitmap(1, 1);
+            Graphics drawing = Graphics.FromImage(img);
+
+            SizeF textSize = drawing.MeasureString(text, fontToMeasure);
+
+            img.Dispose();
+            drawing.Dispose();
+
+            return textSize;
+        }
+
+        
+    }
+}
