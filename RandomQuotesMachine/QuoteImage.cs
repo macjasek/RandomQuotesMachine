6b99b309a811ca6759260de93a06f4fcc163f965
macjasek <maciej.dobruchowski@ispro.com.pl>
2018-01-10 20:06:15 +0100

    Adding calculation of max font size for text as image.

--- RandomQuotesMachine/QuoteImage.cs
+++ RandomQuotesMachine/QuoteImage.cs
@@ -4,18 +4,48 @@ namespace RandomQuotesMachine
 {
     public class QuoteImage
     {
-        public static Image DrawQuote(string text, string fontName, int fontSize)
+        public static Image DrawQuote(string text, string author, string fontName, int fontSize)
         {
 
-            Font drawFont = new Font("Arial", 16);
+            fontSize = CalculateMaxTextSize(text, fontName);
+
+            Font drawFont = new Font(fontName, fontSize);
+
+            text = text.ToUpper();
 
             SizeF textSize = TextSizeCalculator(text, drawFont);
 
-            var img = new Bitmap((int)textSize.Width, (int)textSize.Height);
+            //Recommended image size: 1200 × 628 pixels
+
+            var img = new Bitmap(1200, 628);
 
             var drawing = Graphics.FromImage(img);
 
+            Color backColor = new Color();
+            backColor = Color.FromArgb(0, 205, 207);
+             
 
+            drawing.Clear(backColor);
+
+            Color textColor = Color.White;
+
+            Brush textBrush = new SolidBrush(textColor);
+
+            int xCoordinateOfText = (int)(1200 - textSize.Width)/2;
+            int yCoordinateOfText = (int)(((628 - textSize.Height) / 2) - textSize.Height);
+
+            drawing.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+
+            drawing.DrawString(text, drawFont, textBrush, xCoordinateOfText, yCoordinateOfText);
+
+            drawing.DrawString(author, drawFont, textBrush, xCoordinateOfText, 528);
+
+            drawing.Save();
+
+            textBrush.Dispose();
+            drawing.Dispose();
+
+            img.Save("D:\\test.png",System.Drawing.Imaging.ImageFormat.Png);
 
             return img;
         }
@@ -31,6 +61,26 @@ namespace RandomQuotesMachine
             drawing.Dispose();
 
             return textSize;
+        }
+
+        private static int CalculateMaxTextSize(string text, string fontName)
+        {
+            int maxTextSize = 1;
+
+            Font fontToMeasure = new Font(fontName, maxTextSize);
+
+            int textWidth = 0;
+
+            while (textWidth < 1000)
+            {
+                SizeF textSize = TextSizeCalculator(text.ToUpper(), fontToMeasure);
+                textWidth = (int)textSize.Width;
+                maxTextSize++;
+                fontToMeasure = new Font(fontName, maxTextSize);
+            }
+
+
+            return maxTextSize;
         }
 
         
